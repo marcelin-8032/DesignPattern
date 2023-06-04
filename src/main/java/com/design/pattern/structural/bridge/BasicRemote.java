0fc3adc62e28fdc0c60cef910f8a14c819e486b3
Mohsen Dahesh <mdehesh@gmail.com>
2023-06-04 20:40:19 +0200

    add a bridge design pattern and log4j2 file

---
+++ src/main/java/com/design/pattern/structural/bridge/BasicRemote.java
@@ -0,0 +1,51 @@
+package com.design.pattern.structural.bridge;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class BasicRemote implements Remote {
+
+    protected Device device;
+
+    public BasicRemote() {
+    }
+
+    public BasicRemote(Device device) {
+        this.device = device;
+    }
+
+    @Override
+    public void power() {
+        log.info("Remote: power toggle");
+        if (device.isEnabled()) {
+            device.disable();
+        } else {
+            device.enable();
+        }
+
+    }
+
+    @Override
+    public void volumeDown() {
+        log.info("Remote: volume down");
+        device.setVolume(device.getVolume() - 10);
+    }
+
+    @Override
+    public void volumeUp() {
+        log.info("Remote: volume up");
+        device.setVolume(device.getVolume() + 10);
+    }
+
+    @Override
+    public void channelDown() {
+        log.info("Remote: channel down");
+        device.setVolume(device.getChannel() - 1);
+    }
+
+    @Override
+    public void channelUp() {
+        log.info("Remote: channel up");
+        device.setVolume(device.getChannel() + 1);
+    }
+}
---
+++ src/main/java/com/design/pattern/structural/bridge/BridgeDemo.java
@@ -0,0 +1,29 @@
+package com.design.pattern.structural.bridge;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class BridgeDemo {
+
+
+    public static void main(String[] args) {
+        testDevice(new Tv());
+        testDevice(new Radio());
+    }
+
+    private static void testDevice(Device device) {
+        log.info("Tests with basic remote.");
+        var basicRemote=new BasicRemote(device);
+        basicRemote.power();
+        device.printStatus();
+
+        log.info("Tests with advanced remote.");
+        var advancedRemote=new AdvancedRemote(device);
+        advancedRemote.power();
+        advancedRemote.mute();
+        device.printStatus();
+
+    }
+
+
+}
